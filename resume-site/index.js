0c3024bcbb9ab6572108bdeed0d37911a68474b7
Kamata954 <kimani.walker954@gmail.com>
2025-06-24 00:57:54 -0400

    Redeployed Resume

--- resume-site/index.js
+++ resume-site/index.js
@@ -1,4 +1,4 @@
-fetch("https://xh97jyjxdj.execute-api.us-east-1.amazonaws.com/Prod/hello")
+fetch("https://61noi7v3v9.execute-api.us-east-1.amazonaws.com/Prod/hello")
     .then(response => response.json())
     .then((data) => {
         document.getElementById("replaceme").innerText = data.count
